43a9da262c1e91b9ed0c6b40a3ff948cd17b359a
cruz-Help-Link <itzdonnacruz00@gmail.com>
2024-08-19 09:44:09 -0500

    Exceptions created

---
+++ store/src/main/java/org/cruz/ecommerce/store/exception/ApiError.java
@@ -0,0 +1,10 @@
+package org.cruz.ecommerce.store.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.CONFLICT)
+public class ApiError {
+
+    public ApiError(String message){super();}
+}
---
+++ store/src/main/java/org/cruz/ecommerce/store/exception/ApiException.java
@@ -0,0 +1,10 @@
+package org.cruz.ecommerce.store.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class ApiException extends RuntimeException {
+    public ApiException(String message){super(message);}
+    public ApiException(){super("api error occurred");}
+}
